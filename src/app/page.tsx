88cf64281bc0ab6c28e2179923e2171f310d8c9e
Jaden <jadenday20@gmail.com>
2023-11-20 20:21:36 -0700

    took out an annoying underline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <body className='min-h-screen'>
     <Header/>
     <main className="flex flex-col items-center justify-between p-24">
-      <Link href="/dashboard/" className="bg-blue-600 text-white p-4 rounded-lg underline hover:bg-blue-500 duration-500">This is a test link</Link>
+      <Link href="/dashboard/" className="bg-blue-600 text-white p-4 rounded-lg hover:bg-blue-500 duration-500">This is a test link</Link>
     </main>
     <Footer/>
     </body>
